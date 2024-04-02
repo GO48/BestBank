e5c6811d6a6735f5e3bfab10485bfe3b6f2673d5
MikaShen <mikashen2014@gmail.com>
2024-04-01 22:40:17 -0400

    complete goals

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 A online banking website.
 
 My scrimba bootcamp solo project in module 4.
-I build it from scratch and use all the tricks and great practice I learned so far. I also learned from building: 
+I build it from scratch and use all the tricks and great practice.
+
+I learned so far. I also learned from building: 
 - animation effect
 
 
 ## Feture
-- [ ] fetch data from js
-- [ ] column with spending bars
-- [ ] click on account to change view 
-- [ ] animated hover effect
+- [x] animated hover effect
+- [x] column with spending bars
+- [x] fetch data from js
+- [x] click on account to change view 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* Data */
 const accounts = [
     {
         id: 1,
@@ -52,3 +53,76 @@ const accounts = [
         spendings: []
     }
 ]
+
+/* DOM */
+const accountsEl = document.getElementById("accounts")
+const spendingsEl = document.getElementById("spendings")
+
+/* onload Events */
+window.addEventListener("load", renderAccounts())
+
+/* Functions */
+function renderAccounts() {
+    accounts.forEach((account) => {
+        const newAccount = document.createElement("div")
+        newAccount.className = "account"
+        newAccount.innerHTML = `
+            <p>${account.title}</p>
+            <p>$ ${account.balance}</p>
+        `
+        accountsEl.appendChild(newAccount)
+        newAccount.addEventListener("click", function(){
+            clearActive(accountsEl)
+            changeView()
+            newAccount.classList.add("active")
+            spendingsEl.innerHTML = "<h2>Spendings</h2>"
+            if (account.spendings.length > 0) {
+                renderSpendings(account.id)
+            } else {
+                spendingsEl.innerHTML += "<p>No spendings in this account.</p>"
+            }
+        })
+    })
+}
+
+
+function clearActive(parentEl) {
+    for (const child of parentEl.children) {
+        if (child.classList.contains("active")){
+            child.classList.remove("active")
+        }
+    }
+}
+
+function changeView() {
+    if (accountsEl.classList.contains("in-one-row")) {
+        accountsEl.classList.remove("in-one-row")
+    } 
+    if(spendingsEl.classList.contains("hidden")) {
+        spendingsEl.classList.remove("hidden")
+    }
+}
+
+function renderSpendings(id) {
+    let decreasedWidth = 100
+    accounts[id - 1].spendings.forEach((spending) => {
+        if (decreasedWidth >= 40) decreasedWidth -= 10
+        const newSpending = document.createElement("div")
+        newSpending.className = "spending"
+        newSpending.setAttribute("style", `width: ${decreasedWidth}%;`)
+        newSpending.innerHTML = `
+            <p>${spending.category}</p>
+            <p>${calculatePercentage(spending.spent, accounts[id - 1])}%</p>
+            <p>$ ${spending.spent}</p>
+        `
+        spendingsEl.appendChild(newSpending)
+    })
+}
+
+function calculatePercentage(spent, account) {
+    let spentSum = 0
+    account.spendings.forEach((spending) => {
+        spentSum += parseInt(spending.spent)
+    })
+    return Math.floor(spent / spentSum * 100)
+}
