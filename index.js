ed2bd83233eb53d38115f0c142332494c61ccfaf
MikaShen <mikashen2014@gmail.com>
2024-04-04 01:01:10 -0400

    separate data.js

--- index.js
+++ index.js
@@ -1,65 +1,12 @@
-/* Data */
-const accounts = [
-    {
-        id: 1,
-        title: "Main Account",
-        balance: "6700.56",
-        spendings: [
-            {
-                category: "Rent",
-                spent: "1450"
-            },
-            {
-                category: "Groceries",
-                spent: "564"
-            },
-            {
-                category: "Restaurants",
-                spent: "123"
-            },
-            {
-                category: "Transport",
-                spent: "81"
-            },
-            {
-                category: "Internet",
-                spent: "50"
-            }
-        ]
-    },
-    {
-        id: 2,
-        title: "Expenses",
-        balance: "5134.63",
-        spendings: [
-            {
-                category: "Netflix",
-                spent: "19.99"
-            },
-            {
-                category: "HBO Max",
-                spent: "14.99"
-            },
-            {
-                category: "Setapp",
-                spent: "9.99"
-            }
-        ]
-    },
-    {
-        id: 3,
-        title: "Savings",
-        balance: "36500.12",
-        spendings: []
-    }
-]
+/* import */
+import { accounts } from "./data.js"
 
 /* DOM */
 const accountsEl = document.getElementById("accounts")
 const spendingsEl = document.getElementById("spendings")
 
 /* onload Events */
-window.addEventListener("load", renderAccounts())
+window.addEventListener("load", renderAccounts)
 
 /* Functions */
 function renderAccounts() {
